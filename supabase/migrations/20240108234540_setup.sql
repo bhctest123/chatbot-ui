a551abd11e8aa341cdb6373912a5fd57b8c853e8
bhctest123 <102397029+bhctest123@users.noreply.github.com>
2024-01-28 20:50:02 -0800

    Update 20240108234540_setup.sql

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,6 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'https://iigdafpbpyfjruayiktu.supabase.co';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpZ2RhZnBicHlmanJ1YXlpa3R1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNjUwMTU4NCwiZXhwIjoyMDIyMDc3NTg0fQ.traSrzqjPmuVzmWlxDyRHG0CEhpaRZWbZ1GZZ7t1Tk4'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
